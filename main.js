8d493bb20a348486fbce9f14af870718c79f1c5c
ayuba pelumi <132446974+adebart@users.noreply.github.com>
2023-08-25 15:22:24 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+function click1(){
+    document.getElementById('dtr').style.display ='block'
+    document.getElementById('dtfr').style.display ='block'
+    document.getElementById('dtro').style.display ='block'
+    document.getElementById('dtgr').style.display ='block'
+    document.getElementById('uio').style.display ='block'
+    document.getElementById('tyu').style.display ='none'
+}
+function click2(){
+    document.getElementById('dtr').style.display ='none'
+    document.getElementById('dtfr').style.display ='none'
+    document.getElementById('dtro').style.display ='none'
+    document.getElementById('dtgr').style.display ='none'
+    document.getElementById('uio').style.display ='none'
+    document.getElementById('tyu').style.display ='block'
+}
+
+
+function click3(){
+    document.getElementById('one').style.display ='block'
+    document.getElementById('two').style.display ='block'
+    document.getElementById('three').style.display ='block'
+    document.getElementById('four').style.display ='block'
+    document.getElementById('six').style.display ='block'
+    document.getElementById('five').style.display ='none'
+}
+function click4(){
+    document.getElementById('one').style.display ='none'
+    document.getElementById('two').style.display ='none'
+    document.getElementById('three').style.display ='none'
+    document.getElementById('four').style.display ='none'
+    document.getElementById('six').style.display ='none'
+    document.getElementById('five').style.display ='block'
+}
+
+
+function click5(){
+    document.getElementById('pop').style.display ='block'
+    document.getElementById('ppp').style.display ='block'
+    document.getElementById('popp').style.display ='block'
+    document.getElementById('oppp').style.display ='block'
+    document.getElementById('oppo').style.display ='block'
+    document.getElementById('opop').style.display ='none'
+}
+function click6(){
+    document.getElementById('pop').style.display ='none'
+    document.getElementById('ppp').style.display ='none'
+    document.getElementById('popp').style.display ='none'
+    document.getElementById('oppp').style.display ='none'
+    document.getElementById('oppo').style.display ='none'
+    document.getElementById('opop').style.display ='block'
+}
